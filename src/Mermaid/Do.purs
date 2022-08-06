8e129aba01f4d60de2402430e8437483b2d62ace
Justin Garcia <functor@duck.com>
2022-08-06 11:45:26 +0800

    Automatic voiding for bind/discard

--- src/Mermaid/Do.purs
+++ src/Mermaid/Do.purs
@@ -1,12 +1,13 @@
 module Mermaid.Do (bind, discard, module E) where
 
+import Control.Alt (void)
 import Control.Alternative (pure) as E
 import Control.Bind (class Bind)
 import Data.Unit (Unit)
 import Prelude as Prelude
 
-bind :: forall a m. Bind m => m a -> (a -> m Unit) -> m Unit
-bind = Prelude.bind
+bind :: forall a b m. Bind m => m a -> (a -> m b) -> m Unit
+bind m f = void (Prelude.bind m f)
 
-discard :: forall a m. Bind m => m a -> (a -> m Unit) -> m Unit
-discard = bind
+discard :: forall a b m. Bind m => m a -> (a -> m b) -> m Unit
+discard m f = void (bind m f)
